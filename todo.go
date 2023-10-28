21678f0d557170814df31e1389b979aadbbec7b5
SaveliiYam <savelkayamil5@gmail.com>
2023-10-28 04:41:07 +0300

    28.10 add items

--- todo.go
+++ todo.go
@@ -26,7 +26,6 @@ type ListItem struct {
 	UserId int
 	ItemId int
 }
-
 type UpdateListInput struct {
 	Title       *string `json:"title"`
 	Description *string `json:"description"`
@@ -34,6 +33,19 @@ type UpdateListInput struct {
 
 func (i UpdateListInput) Validate() error {
 	if i.Title == nil && i.Description == nil {
+		return errors.New("update structure has no values")
+	}
+	return nil
+}
+
+type UpdateItemInput struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	Done        *bool   `json:"done"`
+}
+
+func (i UpdateItemInput) Validate() error {
+	if i.Title == nil && i.Description == nil && i.Done == nil {
 		return errors.New("update structure has no values")
 	}
 	return nil
